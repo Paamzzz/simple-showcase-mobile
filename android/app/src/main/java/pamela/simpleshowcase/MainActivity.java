86557b6f095040a583811565c91590afe574db6d
Paamzzz <pamamanciogoulart@gmail.com>
2026-05-21 21:34:33 -0300

    style: melhoria de responsividade

---
+++ android/app/src/main/java/pamela/simpleshowcase/MainActivity.java
@@ -0,0 +1,5 @@
+package pamela.simpleshowcase;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'pamela.simpleshowcase',
+  appName: 'App da Pam',
+  webDir: 'www'
+};
+
+export default config;
