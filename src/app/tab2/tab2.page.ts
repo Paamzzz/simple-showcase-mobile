ecb4d13d8757d3f42a134005ef804889ce3db5a8
Paamzzz <pamamanciogoulart@gmail.com>
2026-05-21 21:01:41 -0300

    feat: tab3 feita

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { NgIf } from '@angular/common';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton } from '@ionic/angular/standalone';
+import { ProdutosService } from '../service/produtos.service';
+import { Produto } from '../service/produtos.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton, NgIf]
 })
 export class Tab2Page {
+     produtos: Produto[] = [];
+     index = 0;
 
-  constructor() {}
+  get produto() { return this.produtos[this.index]; }
+  get isFirst() { return this.index === 0; }
+  get isLast()  { return this.index === this.produtos.length - 1; }
+
+  constructor(private svc: ProdutosService) {}
+
+  ngOnInit() {
+    this.svc.getProdutos().subscribe(p => this.produtos = p);
+  }
+  anterior() { if (!this.isFirst) this.index--; }
+  proximo()  { if (!this.isLast)  this.index++; }
 
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { NgIf, NgFor } from '@angular/common';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton } from '@ionic/angular/standalone';
+import { ProdutosService } from '../service/produtos.service';
+import { Produto } from '../service/produtos.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports:  [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton, NgIf, NgFor]
 })
 export class Tab3Page {
-  constructor() {}
+  todos: Produto[] = [];
+  mostrar = false;
+
+  constructor(private svc: ProdutosService) {}
+
+  verTodos() {
+    this.svc.getProdutos().subscribe(p => {
+      this.todos = p;
+      this.mostrar = true;
+    });
+  }
+  esconder() {
+    this.mostrar = false;
+    this.todos = [];
+  }
 }
